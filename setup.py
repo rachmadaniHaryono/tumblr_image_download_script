548001c25fa900c25e10b4a93065a12fcb94922d
rachmadaniHaryono <foreturiga@gmail.com>
2017-01-02 10:51:31 +0800

    change scripts and entry point on setup.

--- setup.py
+++ setup.py
@@ -10,8 +10,10 @@ setup(
     version='0.1',
     author='leyle',
     author_email='jrh@example.com',
-    # packages=['towelstuff', 'towelstuff.test'],
-    scripts=['general_run.py'],
+    scripts=['bin/tumblr-ids'],
+    entry_points = {
+        'console_scripts': ['tumblr-ids=tumblr_ids.general_run:main'],
+    },
     url='https://github.com/Bakkingamu/tumblr_image_download_script',
     license='LICENSE.txt',
     description='Download images from a tumblr blog (all or certain tags).',
