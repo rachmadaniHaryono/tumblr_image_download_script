8c1e2d0858182a79a7b0a4af86f19cceafb5494c
rachmadaniHaryono <foreturiga@gmail.com>
2017-07-04 06:41:14 +0800

    chg: dev: update setup
    - use setuptools
    - use semantic version
    - fix license keyword
    - change long_description to rst readme
    - add classifiers, zip_safe and keywords to meet pyroma spec
    - add required package here

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ python setup file.
 taken from
 http://the-hitchhikers-guide-to-packaging.readthedocs.io/en/latest/creation.html#setup-py-descriptiono
 """
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+install_requires = (
+    'requests>=2.12.4'
+)
 
 setup(
     name='tumblr_image_download_script',
-    version='0.1',
+    version='0.1.0',
     author='leyle',
     author_email='jrh@example.com',
     scripts=['bin/tumblr-ids'],
@@ -15,10 +23,18 @@ setup(
         'console_scripts': ['tumblr-ids=tumblr_ids.__main__'],
     },
     url='https://github.com/Bakkingamu/tumblr_image_download_script',
-    license='LICENSE.txt',
+    license='MIT License',
     description='Download images from a tumblr blog (all or certain tags).',
-    long_description=open('README.md').read(),
-    # install_requires=[],
+    long_description=open('README.rst').read(),
+    install_requires=install_requires,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License'
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    zip_safe=False,
+    keywords='tumblr downloader cli'
 )
